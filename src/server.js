56bd1135ced7a007392b9e1cbda1176c99edfd89
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-14 19:22:07 +0300

    feat: add morgan for better log

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import createTable from "./migrations/tables.js";
 import { config } from "dotenv";
+import morgan from "morgan";
 
 config();
 const app = express();
+app.use(morgan("dev"));
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.status(200).send("Hello world");
 });
 
 app.listen(port, () => {
