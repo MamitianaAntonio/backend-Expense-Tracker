22976c21ce9bc5591fcf814d804a2c3ea97cd69d
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-21 11:03:37 +0300

    feat: auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+
+config();
+
+export const authenticateUser = (req, res, next) => {
+  const token = req.header("Authaurization").replace("Bearer ", "");
+  if (!token) {
+    res.status(401).json({
+      message: "Please create an account or log in",
+    });
+  } else {
+    try {
+      const decodedInformation = jwt.verify(token, process.env.SECRET_KEY);
+      req.user = decodedInformation;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).json({
+          message: "Expired token.",
+        });
+      }
+      return res.status(401).json({
+        message: "Invalid token.",
+      });
+    }
+  }
+};
