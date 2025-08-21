4b6d70fb185545fdbe5f6a5f87ee8f31674e057f
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-21 11:59:51 +0300

    refactor: wrap in trycatch bloc

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -2,19 +2,24 @@ import { createUserQuery, getUsersQuery } from "../utils/sql/users.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-  console.log(req.body);
-  const { email, password } = req.body;
-  const users = await getUsersQuery();
-  if (users.rows.some((user) => user.email === email)) {
-    return res.status(409).json({
-      message: "You cannot create account twice.",
+  try {
+    const { email, password } = req.body;
+    const users = await getUsersQuery();
+    if (users.rows.some((user) => user.email === email)) {
+      return res.status(409).json({
+        message: "You cannot create account twice.",
+      });
+    }
+    const hachedPassword = await bcrypt.hash(password, 10);
+    const accountCreated = await createUserQuery(email, hachedPassword);
+
+    // BETA TEST.
+    res.status(201).json({
+      message: "user created",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
     });
   }
-  const hachedPassword = await bcrypt.hash(password, 10);
-  const accountCreated = await createUserQuery(email, hachedPassword);
-
-  // BETA TEST.
-  res.status(201).json({
-    message: "user created",
-  });
 };
