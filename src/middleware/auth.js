f2ffaf21015ff73f0dc6068c1a2d70059df28424
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-21 11:07:22 +0300

    fix: token safety checker

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,12 +4,16 @@ import { config } from "dotenv";
 config();
 
 export const authenticateUser = (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
-  if (!token) {
+  const tokenAuth = req.header("Authorization");
+  if (!tokenAuth) {
     res.status(401).json({
       message: "Please create an account or log in",
     });
   } else {
+    let token = tokenAuth.startsWith("Bearer ")
+      ? tokenAuth.slice(7)
+      : tokenAuth;
+
     try {
       const decodedInformation = jwt.verify(token, process.env.SECRET_KEY);
       req.user = decodedInformation;
