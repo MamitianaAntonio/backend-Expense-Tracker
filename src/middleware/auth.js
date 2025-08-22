bba61ff453daefbe288758929dbda287e517042a
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-22 11:41:56 +0300

    fix: localStorage verify to Cookie

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,16 +4,12 @@ import { config } from "dotenv";
 config();
 
 export const authenticateUser = (req, res, next) => {
-  const tokenAuth = req.header("Authorization");
+  const tokenAuth = req.cookies.access;
   if (!tokenAuth) {
     res.status(401).json({
       message: "Please create an account or log in",
     });
   } else {
-    let token = tokenAuth.startsWith("Bearer ")
-      ? tokenAuth.slice(7)
-      : tokenAuth;
-
     try {
       const decodedInformation = jwt.verify(token, process.env.SECRET_KEY);
       req.user = decodedInformation;
