97746d39ec31b9df674966c74f12137549aaebe0
Zinedis <nantenainabakari@gmail.com>
2025-08-24 19:51:39 +0200

    Feat [add] function getAllExpenses in expense.routes

---
+++ src/routes/expense.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { getAllExpenses, getExpenseById, createExpense, updateExpense, deleteExpense } = require("../controllers/expenseController.js");
+const requireAuth = require("../middleware/authMiddleware.js");
+const upload = require("../middleware/multer.middleware.js");
+
+const router = express.Router();
+
+router.use(requireAuth);
+
+router.get("/", getAllExpenses);
+router.get("/:id", getExpenseById);
+router.post("/", upload.single("file"), createExpense);
+router.put("/:id", upload.single("file"), updateExpense);
+router.delete("/:id", deleteExpense);
+
+module.exports = router;
