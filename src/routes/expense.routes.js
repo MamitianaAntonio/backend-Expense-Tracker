7ba4a53a6032e644ea8f12d7ef2c4ef2e70aee58
Zinedis <nantenainabakari@gmail.com>
2025-08-24 19:58:29 +0200

    Feat [add] function getExpenseById in expense.routes

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.use(requireAuth);
 
 router.get("/", getAllExpenses);
-// router.get("/:id", getExpenseById);
+router.get("/:id", getExpenseById);
 // router.post("/", upload.single("file"), createExpense);
 // router.put("/:id", upload.single("file"), updateExpense);
 // router.delete("/:id", deleteExpense);
