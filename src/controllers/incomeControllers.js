5afd1136d64982d6e31c2e18d21dd16f967247cc
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-25 11:09:37 +0300

    fix: $ in query were false

---
+++ src/controllers/incomeControllers.js
@@ -0,0 +1,19 @@
+import { getIncomesQuery } from "../utils/sql/incomes.js";
+
+export const getIncomes = async (req, res) => {
+  try {
+    const email = req.user.email;
+    const start_date = req.query.start;
+    const end_date = req.query.end;
+    let listOfIncomes;
+    if (start_date == undefined && end_date == undefined) {
+      const resultSet = await getIncomesQuery(email);
+      listOfIncomes = resultSet.rows;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
--- src/utils/sql/incomes.js
+++ src/utils/sql/incomes.js
@@ -1,6 +1,6 @@
 import { pool } from "../../config/db.js";
 
-export const getIncomes = (email) => {
+export const getIncomesQuery = (email) => {
   return pool.query(
     `
     select i.*, u.email, u.username from income as i inner join users u on u.id = i.user_id where u.email=$1
@@ -9,10 +9,10 @@ export const getIncomes = (email) => {
   );
 };
 
-export const getIncomesBtwDate = (email, start_date, end_date) => {
+export const getIncomesBtwDateQuery = (email, start_date, end_date) => {
   return pool.query(
     `
-    select i.*, u.email, u.username from income as i inner join users u on u.id = i.user_id where u.email=$1 and current_date between $1 and $2;
+    select i.*, u.email, u.username from income as i inner join users u on u.id = i.user_id where u.email=$1 and current_date between $2 and $3;
 `,
     [email, start_date, end_date],
   );
