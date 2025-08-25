c34602cec6a4825dc6f9aa34715ef59b2a7dabce
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-25 11:19:42 +0300

    feat: get controller

--- src/controllers/incomeControllers.js
+++ src/controllers/incomeControllers.js
@@ -1,4 +1,9 @@
-import { getIncomesQuery } from "../utils/sql/incomes.js";
+import {
+  getIncomesBtwDateQuery,
+  getIncomesQuery,
+  getIncomesWthEndDateQuery,
+  getIncomesWthStartDateQuery,
+} from "../utils/sql/incomes.js";
 
 export const getIncomes = async (req, res) => {
   try {
@@ -9,7 +14,25 @@ export const getIncomes = async (req, res) => {
     if (start_date == undefined && end_date == undefined) {
       const resultSet = await getIncomesQuery(email);
       listOfIncomes = resultSet.rows;
+    } else if (start_date != undefined || end_date != undefined) {
+      if (start_date == undefined && end_date != undefined) {
+        const resultSet = await getIncomesWthEndDateQuery(email, end_date);
+        listOfIncomes = resultSet.rows;
+      } else if (start_date != undefined && end_date == undefined) {
+        const resultSet = await getIncomesWthStartDateQuery(email, start_date);
+        listOfIncomes = resultSet.rows;
+      } else {
+        const resultSet = await getIncomesBtwDateQuery(
+          email,
+          start_date,
+          end_date,
+        );
+        listOfIncomes = resultSet.rows;
+      }
     }
+    res.status(200).json({
+      data: listOfIncomes,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
