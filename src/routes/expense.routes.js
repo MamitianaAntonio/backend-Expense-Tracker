e817cd939e78dc622bd980227aeb43dd2735f1f8
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-29 18:03:18 +0300

    fix: add post and put expense routes.

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getAllExpenses, getExpenseById, deleteExpense } from "../controllers/expense.controller.js";
+import { getAllExpenses, getExpenseById, deleteExpense, createExpense, updateExpense } from "../controllers/expense.controller.js";
 import { authenticateUser } from "../middleware/auth.js";
 
 const router = Router();
 
 router.get("/", authenticateUser, getAllExpenses);
 router.get("/:id", authenticateUser, getExpenseById);
+router.post("/", authenticateUser, createExpense);
+router.put("/:id", authenticateUser, updateExpense);
 router.delete("/:id", authenticateUser, deleteExpense);
 
 export default router;
