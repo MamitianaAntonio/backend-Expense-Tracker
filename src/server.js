5f60850129add4f6d6c7315b7fdcdab992eebae1
Zinedis <nantenainabakari@gmail.com>
2025-09-05 00:06:02 +0200

    Feat update server.js

--- src/server.js
+++ src/server.js
@@ -1,36 +1,34 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const path = require("path");
-require("dotenv").config();
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import authRoute from './routes/authRoute.js';
+import expenseRoute from './routes/expenseRoute.js';
+
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
-const authRoutes = require("./routes/authRoutes.js");
-const expenseRoutes = require("./routes/expense.routes.js");
-
-
-//authorized cookie
 app.use(cors({
-  origin: process.env.ORIGIN || "http://localhost:3000",
+  origin: process.env.ORIGIN || 'http://localhost:3000',
   credentials: true,
 }));
-
-// static for uploads
-app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
-
-app.get("/", (req, res) => res.json({ status: "ok", service: "expense-app-sql-backend" }));
-
-app.use("/api/auth", authRoutes);
-app.use("/api/expenses", expenseRoutes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'Uploads')));
+app.get('/', (req, res) => res.json({ status: 'ok', service: 'expense-app-sql-backend' }));
+app.use('/api/auth', authRoute);
+app.use('/api/expenses', expenseRoute);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, (res, error) => {
-  res.console.log(`Server running on http://localhost:${PORT}`)
-  if(error){
-    console.error(error);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Error starting server:', err);
+    process.exit(1);
   }
+  console.log(`Server running on http://localhost:${PORT}`);
 });
